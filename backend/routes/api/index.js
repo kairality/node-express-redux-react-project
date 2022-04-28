9cef045a77491aa31748d529379c493f680a70b0
Kai Seward <kairality@gmail.com>
2022-04-28 12:04:51 -0700

    auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'MintBananas'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
 
 module.exports = router;
---
+++ backend/utils/auth.js
@@ -0,0 +1,62 @@
+// sets a jwt cookie and returns it
+const setTokenCookie = (res, user) => {
+  // Create the token.
+  const token = jwt.sign(
+    { data: user.toSafeObject() },
+    secret,
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+  );
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  // Set the token cookie
+  res.cookie("token", token, {
+    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && "Lax",
+  });
+
+  return token;
+};
+
+
+const restoreUser = (req, res, next) => {
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope("currentUser").findByPk(id);
+    } catch (e) {
+      res.clearCookie("token");
+      return next();
+    }
+
+    if (!req.user) res.clearCookie("token");
+
+    return next();
+  });
+};
+
+// try to restore the user from token, if unauthorized return an error
+const requireAuth = [
+  restoreUser,
+  function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error("Unauthorized");
+    err.title = "Unauthorized";
+    err.errors = ["Unauthorized"];
+    err.status = 401;
+    return next(err);
+  },
+];
+
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
