8721eb26c3ae987ad5f0b2545fd3f56e8f228266
Kai Seward <kairality@gmail.com>
2022-04-28 10:53:35 -0700

    error handling code

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
 
 // internal requires
 const routes = require("./routes");
@@ -45,6 +46,37 @@ app.use(
 
 // use all of our routes
 app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  // next with an error invokes following error handling middlewares
+  // next with no error does NOT invoke following middleweares
+  next(err);
+});
+
+// handle sequelize errors of type ValidationError
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Format errors
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 // export time!
 module.exports = app;
