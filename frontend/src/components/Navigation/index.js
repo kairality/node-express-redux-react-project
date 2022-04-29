09107e383a2285d2f1928c90b4db928477e0384b
Kai Seward <kairality@gmail.com>
2022-04-29 12:51:32 -0700

    modals for login/signup

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,38 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton.js";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SIgnupFormModal";
+import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
-
-
   const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
-
   if (sessionUser) {
     sessionLinks = <ProfileButton user={sessionUser} />;
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <LoginFormModal />
+        <SignupFormModal />
       </>
     );
   }
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-          {isLoaded && sessionLinks}
-        </li>
-      </ul>
-    </nav>
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
   );
 }
 
