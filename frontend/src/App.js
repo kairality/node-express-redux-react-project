ad0ef822a5874b590ae40c4cd8e5142af21b4964
Kai Seward <kairality@gmail.com>
2022-04-29 12:26:10 -0700

    login working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,35 @@
+import {Route, Switch} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import { useState, useEffect } from 'react';
+import { sessionRestoreUser } from './store/session';
+
+import Navigation from './components/Navigation';
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from './components/SignUpFormPage';
+
 function App() {
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+      dispatch(sessionRestoreUser()).then(() => setIsLoaded(true));
+    }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    isLoaded && (
+      <>
+        <Navigation isLoaded={isLoaded} />
+        <h1>Hello from application</h1>
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      </>
+    )
   );
 }
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -26,6 +26,29 @@ export const loginUser = (user) => async (dispatch) => {
     return response;
 }
 
+export const signupUser = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const signupUserBody = JSON.stringify({username, email, password});
+  const response = await ezFetch('/api/users', "POST", signupUserBody);
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const sessionRestoreUser = () => async (dispatch) => {
+  const response = await ezFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const logoutUser = () => async (dispatch) => {
+  const response = await ezFetch("/api/session", "DELETE");
+  // remove the user
+  dispatch(removeUser());
+  return response;
+};
+
 const loggedOutUserState = { user : null };
 
 const sessionReducer = (state = loggedOutUserState, action) => {
